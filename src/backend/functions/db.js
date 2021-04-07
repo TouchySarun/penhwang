19b8d21306739237bb996b76abd3db69aa78a9c6
TouchySarun <35326638+TouchySarun@users.noreply.github.com>
2021-04-07 11:10:20 +0700

    sensor keys

--- src/backend/functions/db.js
+++ src/backend/functions/db.js
@@ -1,14 +1,14 @@
 const admin = require('firebase-admin');
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDXHSdq1JoWVzG24fBlr82gECDERMQjmy8",
-  authDomain: "penhwang-d350c.firebaseapp.com",
-  databaseURL: "https://penhwang-d350c.firebaseio.com",
-  projectId: "penhwang-d350c",
-  storageBucket: "penhwang-d350c.appspot.com",
-  messagingSenderId: "460959132300",
-  appId: "1:460959132300:web:05dc2319b735c21cf329a9",
-  measurementId: "G-XRMW2G5Q00"
+  apiKey: "**********",
+  authDomain: "**********",
+  databaseURL: "**********",
+  projectId: "**********",
+  storageBucket: "**********",
+  messagingSenderId: "**********",
+  appId: "**********",
+  measurementId: "**********"
 };
 
 admin.initializeApp(firebaseConfig);
